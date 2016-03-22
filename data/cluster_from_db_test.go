c6453e62273640f88194b69371f31228bc180259
Aaron Schlesinger <aschlesinger@deis.com>
2016-03-22 12:06:12 -0700

    fix(cluster_from_db_test.go): add test for cluster checkin
    also, move the test cluster creation code into a utility func

--- data/cluster_from_db_test.go
+++ data/cluster_from_db_test.go
@@ -8,18 +8,8 @@ import (
 	"github.com/deis/workflow-manager/types"
 )
 
-func TestClusterFromDBRoundTrip(t *testing.T) {
-	db, err := newMemDB()
-	assert.NoErr(t, err)
-	sqliteDB, err := db.Get()
-	assert.NoErr(t, err)
-
-	assert.NoErr(t, VerifyPersistentStorage(db))
-	c := ClusterFromDB{}
-	cluster, err := c.Get(sqliteDB, clusterID)
-	assert.True(t, err != nil, "error not returned when expected")
-	assert.Equal(t, cluster, types.Cluster{}, "returned cluster")
-	expectedCluster := types.Cluster{
+func testCluster() types.Cluster {
+	return types.Cluster{
 		ID:        clusterID,
 		FirstSeen: time.Now(),
 		LastSeen:  time.Now().Add(1 * time.Hour),
@@ -37,10 +27,39 @@ func TestClusterFromDBRoundTrip(t *testing.T) {
 			},
 		},
 	}
+}
+
+func TestClusterFromDBRoundTrip(t *testing.T) {
+	db, err := newMemDB()
+	assert.NoErr(t, err)
+	sqliteDB, err := db.Get()
+	assert.NoErr(t, err)
+
+	assert.NoErr(t, VerifyPersistentStorage(db))
+	c := ClusterFromDB{}
+	cluster, err := c.Get(sqliteDB, clusterID)
+	assert.True(t, err != nil, "error not returned when expected")
+	assert.Equal(t, cluster, types.Cluster{}, "returned cluster")
+	expectedCluster := testCluster()
 	setCluster, err := c.Set(sqliteDB, clusterID, expectedCluster)
 	assert.NoErr(t, err)
 	assert.Equal(t, setCluster.ID, expectedCluster.ID, "cluster")
 	getCluster, err := c.Get(sqliteDB, clusterID)
 	assert.NoErr(t, err)
 	assert.Equal(t, getCluster, setCluster, "cluster")
+}
+
+func TestClusterFromDBCheckin(t *testing.T) {
+	db, err := newMemDB()
+	assert.NoErr(t, err)
+	sqliteDB, err := db.Get()
+	assert.NoErr(t, err)
+
+	assert.NoErr(t, VerifyPersistentStorage(db))
+	c := ClusterFromDB{}
+	res, err := c.Checkin(sqliteDB, clusterID, testCluster())
+	assert.NoErr(t, err)
+	rowsAffected, err := res.RowsAffected()
+	assert.NoErr(t, err)
+	assert.Equal(t, rowsAffected, int64(1), "number of rows affected")
 }
