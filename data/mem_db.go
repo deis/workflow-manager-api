9c679cf5a2c6f22f82cee9191b28931b35008013
Aaron Schlesinger <aschlesinger@deis.com>
2016-04-19 16:06:16 -0700

    feat(handlers,server.go): add endpoint to get latest release for specified component/train
    fix(server,handlers/latest_component_train.go): finish implementation of the handler
    
    fix(data): implement Latest on the VersionFromDB interface implementation
    
    also, adding tests
    
    fix(data): exporting creation func for in-memory DBs
    
    in preparation for testing handlers
    
    ref(data): add func to parse a single ComponentVersion from a VersionsTable
    
    this is in preparation for use inside the Latest func
    
    ref(data): use parseDBVersion from the Latest func
    
    fix(data): add more comprehensive tests for the version Latest func
    
    fix(handlers): add test skeleton for the latest component handler
    
    doc(api-architecture.md): remove not yet implemented note
    
    doc(api-architecture.md): remove description from response
    
    it will not be returned because we don’t currently store it in the DB
    
    fix(data/version_from_db_test.go): fix test for version's Latest func)
    
    fix(handlers): remove handler-level test
    
    this test will go into server-level tests
    
    fix(server.go,handlers): merge two handlers that did the same thing
    
    LatestComponentAndTrainHandler and GetLatestComponentTrainVersion do
    the same thing, but were registered twice. this commit merges the
    handlers and only registers them once. also, the routes were re-ordered
    to look for the latest version before a named version. in the opposite
    case, the named version handler was executed, looked for a version
    named “latest” in the DB, and fail
    
    fix(mem_db_test.go,server_test.go): remove the memDB from main
    
    prefer to use the (now-exported) one in the data package
    
    fix(server_test.go): add a test for the latest version/train endpoint
    
    fix(Makefile,data): protect sqlite from building with CGO_ENABLED=0
    
    also, document that fact in code
    
    doc(README.md): document requirements and command for running tests with the go tool

---
+++ data/mem_db.go
@@ -0,0 +1,51 @@
+// +build testonly
+
+package data
+
+import (
+	"database/sql"
+	// this import registers "sqlite3" as a name you can provide to sql.Open
+	_ "github.com/mxk/go-sqlite/sqlite3"
+)
+
+// NOTE: we are using a build tag to conditionally build this file (see the comment at the top
+// of this file) because we want to use it in tests, but also want to build with CGO_ENABLED=0.
+// When CGO_ENABLED is set to 0, C source files are not allowed in the build, and since
+// sqlite uses C source files, we can't build this.
+//
+// However, we can run tests with CGO_ENABLED=1, so as long as we run tests with -tags testonly,
+// this file will be included and we we're good to go.
+//
+// Why not put this in a test file (i.e. mem_db_test.go), you ask? In short, symbols in test
+// files are not exported outside of the package they live in, so the in-memory DB logic would
+// have to be copied everywhere it's needed. Right now, that's in this package as well as 'main'.
+// Not the worst thing, but this is cleaner.
+//
+// For more information on build tags, see
+// http://dave.cheney.net/2013/10/12/how-to-use-conditional-compilation-with-the-go-build-tool
+
+const (
+	sqlite3Str = "sqlite3"
+	memStr     = ":memory:"
+)
+
+// memDB is a sqlite in-memory data.DB implementation. note that it's in a *_test.go file since
+// it can't be built without cgo. Since the production binary is built with CGO_ENABLED=0, cgo
+// is off, but *_test.go files are omitted.
+type memDB struct {
+	db *sql.DB
+}
+
+func (m memDB) Get() (*sql.DB, error) {
+	return m.db, nil
+}
+
+// NewMemDB returns a DB implementation that stores all data in-memory. The initial database
+// is empty, and is best used for testing.
+func NewMemDB() (DB, error) {
+	db, err := sql.Open(sqlite3Str, memStr)
+	if err != nil {
+		return nil, err
+	}
+	return &memDB{db: db}, nil
+}
--- data/version_from_db_test.go
+++ data/version_from_db_test.go
@@ -1,7 +1,9 @@
 package data
 
 import (
+	"fmt"
 	"testing"
+	"time"
 
 	"github.com/arschles/assert"
 	"github.com/deis/workflow-manager/types"
@@ -24,7 +26,7 @@ func testComponentVersion() types.ComponentVersion {
 }
 
 func TestVersionFromDBRoundTrip(t *testing.T) {
-	memDB, err := newMemDB()
+	memDB, err := NewMemDB()
 	assert.NoErr(t, err)
 	sqliteDB, err := memDB.Get()
 	assert.NoErr(t, err)
@@ -48,4 +50,46 @@ func TestVersionFromDBRoundTrip(t *testing.T) {
 	assert.Equal(t, getCVer.Version.Version, componentVersion.Version.Version, "version string")
 	assert.Equal(t, getCVer.Version.Released, componentVersion.Version.Released, "released string")
 	assert.Equal(t, getCVer.Version.Train, componentVersion.Version.Train, "version train")
+}
+
+func TestVersionFromDBLatest(t *testing.T) {
+	memDB, err := NewMemDB()
+	assert.NoErr(t, err)
+	sqliteDB, err := memDB.Get()
+	assert.NoErr(t, err)
+	db, err := VerifyPersistentStorage(memDB)
+	assert.NotNil(t, db, "db")
+	assert.NoErr(t, err)
+	ver := VersionFromDB{}
+
+	const numCVs = 4
+	const latestCVIdx = 2
+	componentVersions := make([]types.ComponentVersion, numCVs)
+	for i := 0; i < numCVs; i++ {
+		cv := testComponentVersion()
+		cv.Component.Name = componentName
+		cv.Component.Description = fmt.Sprintf("description%d", i)
+		cv.Version.Train = train
+		cv.Version.Version = fmt.Sprintf("testversion%d", i)
+		cv.Version.Released = time.Now().Add(time.Duration(i) * time.Hour).Format(released)
+		cv.Version.Data = []byte(fmt.Sprintf("data%d", i))
+		if i == latestCVIdx {
+			cv.Version.Released = time.Now().Add(time.Duration(numCVs+1) * time.Hour).Format(released)
+		}
+		if _, setErr := ver.Set(sqliteDB, cv); setErr != nil {
+			t.Fatalf("error setting component version %d (%s)", i, setErr)
+		}
+		componentVersions[i] = cv
+	}
+	cv, err := ver.Latest(sqliteDB, train, componentName)
+	assert.NoErr(t, err)
+	exCV := componentVersions[latestCVIdx]
+	assert.Equal(t, cv.Component.Name, exCV.Component.Name, "component name")
+	// since the versions table doesn't store a description now, make sure it comes back empty
+	assert.Equal(t, cv.Component.Description, "", "component name")
+
+	assert.Equal(t, cv.Version.Train, exCV.Version.Train, "component version")
+	assert.Equal(t, cv.Version.Version, exCV.Version.Version, "component version")
+	assert.Equal(t, cv.Version.Released, exCV.Version.Released, "component release time")
+	assert.Equal(t, string(cv.Version.Data), string(exCV.Version.Data), "component version data")
 }
