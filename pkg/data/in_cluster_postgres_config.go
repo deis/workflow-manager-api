d9c069cb2ee2bd55a14f5ff77029f6c27ce0b786
Aaron Schlesinger <aaron@deis.com>
2016-07-18 11:40:45 -0700

    fix(postgres): implement file i/o convenience functions

--- pkg/data/in_cluster_postgres_config.go
+++ pkg/data/in_cluster_postgres_config.go
@@ -1,6 +1,8 @@
 package data
 
 import (
+	"io/ioutil"
+
 	"github.com/kelseyhightower/envconfig"
 )
 
@@ -11,7 +13,7 @@ const (
 // InClusterPostgresConfig is the envconfig-compatible struct for connecting to an in-cluster postgres DB
 type InClusterPostgresConfig struct {
 	Host         string `envconfig:"DEIS_WFMPOSTGRES_SERVICE_HOST" required:"true"`
-	Post         int    `envconfig:"DEIS_WFMPOSTGRES_SERVICE_PORT" required:"true"`
+	Port         int    `envconfig:"DEIS_WFMPOSTGRES_SERVICE_PORT" required:"true"`
 	UsernameFile string `envconfig:"DEIS_WFMPOSTGRES_USERNAME_FILE" default:"/var/run/secrets/postgres/auth/username"`
 	PasswordFile string `envconfig:"DEIS_WFMPOSTGRES_PASSWORD_FILE" default:"/var/run/secrets/postgres/auth/password"`
 	DBName       string `envconfig:"DEIS_WFMPOSTGRES_DB_NAME" default:"wfm"`
@@ -25,4 +27,24 @@ func GetInClusterPostgresConfig() (*InClusterPostgresConfig, error) {
 		return nil, err
 	}
 	return ret, nil
+}
+
+// GetUsername reads the file at i.UsernameFile and returns its contents as a string.
+// returns a non-nil error if the file is missing or there was any issue reading it
+func (i InClusterPostgresConfig) GetUsername() (string, error) {
+	b, err := ioutil.ReadFile(i.UsernameFile)
+	if err != nil {
+		return "", err
+	}
+	return string(b), nil
+}
+
+// GetPassword reads the file at i.PasswordFile and returns its contents as a string.
+// returns a non-nil error if the file is missing or there was any issue reading it
+func (i InClusterPostgresConfig) GetPassword() (string, error) {
+	b, err := ioutil.ReadFile(i.PasswordFile)
+	if err != nil {
+		return "", err
+	}
+	return string(b), nil
 }
